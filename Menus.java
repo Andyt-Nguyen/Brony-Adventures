0108c87df641aeee7f1862f74535abfd05520682
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2018-11-29 13:51:01 -0700

    menu system

--- Menus.java
+++ Menus.java
@@ -1,13 +1,21 @@
 public class Menus {
+    
     public static void displayHelp() {
-        System.out.println("------------------------------------");
-        System.out.println("How to pick up items: Pick up [item]");
-        System.out.println("View inventory: Inventory");
-        System.out.println("View your surroundings: Look around");
-        System.out.println("View health: Diagnostics");
-        System.out.println("Look under objects: Look under [object]");
-        System.out.println("Fight: Fight [enemy]");
-        System.out.println("------------------------------------");
+        System.out.println("*********************************************");
+        System.out.println("* Command           |    Action               *");
+        System.out.println("*********************************************");
+        System.out.println("  move north          |    Move up               *");
+        System.out.println("  move south          |    Move down             *");
+        System.out.println("  move east           |    Move right            *");
+        System.out.println("  move west           |    Move left             *");
+        System.out.println("  scream              |    Player screams        *");
+        System.out.println("  pick up [item]      |    Player picks up wepon *");
+        System.out.println("  view inventory      |    look in inventory     *");
+        System.out.println("  view surroundings   |    look around room      *");
+        System.out.println("  look under [object] |    looks under object    *");
+        System.out.println("  diagnostics         |    view hp               *");
+        System.out.println("  fight [enemy]       |    fight enemy           *");
+        System.out.println("**************************************************");
     }
 
     public static void displayGameOver(Player player){
