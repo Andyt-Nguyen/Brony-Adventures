ccc3fad5bf214767398d46455b51589d72e6ed72
Sean Vaughan <seanvaughan@Seans-MacBook-Air.local>
2018-11-30 18:10:27 -0700

    Added a bit more decision making

--- Hallway.java
+++ Hallway.java
@@ -86,24 +86,60 @@ public class Hallway extends Room{
     }
 
     public void performAction(Player player, String keyword){
+        boolean userChoice;
         if(keyword.equalsIgnoreCase("lamp") && this.lamp == 1 && this.getRoomID() == 2){
             System.out.println("It's a pony shaped lamp, you decide you're already deathly freightened of ponies and stay away from it.");
             setLamp(0);
-            this.hallwayKeywords.remove("Lamp");
             player.addToPoints(2);
-        }else if(keyword.equalsIgnoreCase("lamp") && this.lamp == 0 && this.getRoomID() == 2){
-            System.err.println("Sorry the command " + keyword + " is not useable here.");
+            this.hallwayKeywords.remove("Lamp");
         }
         if(keyword.equalsIgnoreCase("window") && this.window == 1 && this.getRoomID() == 2){
-            System.out.println("You see a window and decide to make a jump for it, as you're jumping out you get " +
-            " tripped up on your foot, you fall two stories onto your head and die.");
-            player.addToPoints(2);
-            Menus.displayGameOver(player);
-            setWindow(0);
-            player.setHp(0);
-            this.hallwayKeywords.remove("Window");
-        }else if(keyword.equalsIgnoreCase("window") && this.window == 0 && this.getRoomID() == 2){
-            System.err.println("Sorry the command " + keyword + " is not useable here.");
+            System.out.println("You see a window..");
+            userChoice = IR5.getYorN("Would you like to try and escape from this window?");
+            if(userChoice){
+                System.out.println("You see a window and decide to make a jump for it, as you're jumping out you get " +
+                " tripped up on your foot, you fall two stories onto your head and die.");
+                player.addToPoints(2);
+                Menus.displayGameOver(player);
+                setWindow(0);
+                player.setHp(0);
+                this.hallwayKeywords.remove("Window");
+            }else{
+                System.out.println("You decide not to attempt to escape.");
+            }
+        }
+        if(keyword.equalsIgnoreCase("window") && this.window == 1 && this.getRoomID() == 5){
+            System.out.println("\nYou see window..");
+            userChoice = IR5.getYorN("\nWould you like to try to escape through the window?(Y/N)");
+            if(userChoice){
+                System.out.println("\nYou open up the window, you climb out and as you're hanging onto the ledge you notice");
+                System.out.println("that this window is hanging right over a pen of those demonic ponies!");
+                System.out.println("You decide to climb back up but this process has weakened you..");
+                player.decreaseHp(5);
+                player.addToPoints(2);
+                setWindow(0);
+                this.hallwayKeywords.remove("Window");
+            }else{
+                System.out.println("You decide to not try and escape from the window. You take a step back from it.");
+            }
+        }
+        if(keyword.equalsIgnoreCase("statue") && this.statue == 1 && this.getRoomID() == 5){
+            System.out.println("You notice a statue in the corner of the room..");
+            userChoice = IR5.getYorN("It's kind of hard to see from here would you like to get closer?(Y/N)");
+            if(userChoice){
+                System.out.println("\nYou inch closer towards the statue..");
+                System.out.println("As you get closer the statue becomes more and more familiar..");
+                System.out.println("......");
+                System.out.println("You realize that it's a statue of you!");
+                System.out.println("Oddly, this doesn't freak you out and instead you gain some confidence.");
+                System.out.println("Seeing something so beautiful makes you feel stronger.");
+                player.increaseHp(10);
+                player.addToPoints(2);
+                setStatue(0);
+                this.hallwayKeywords.remove("Statue");
+            }else{
+                System.out.println("You decide not to get closer to the statue, it remains in the dark unknown..");
+            }
         }
         if(keyword.equalsIgnoreCase("north") || keyword.equalsIgnoreCase("south") || keyword.equalsIgnoreCase("east") || 
         keyword.equalsIgnoreCase("west")){
